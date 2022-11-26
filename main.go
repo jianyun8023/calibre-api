b40f07d229123c621b7f5b3a076a5a045db8d80b
Jianyun Zhao <zhaojianyun88@outlook.com>
2022-11-26 22:43:39 +0800

    lanzou

--- main.go
+++ main.go
@@ -3,28 +3,63 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/bookstairs/bookhunter/internal/client"
+	"github.com/bookstairs/bookhunter/internal/driver/lanzou"
 	"github.com/gin-gonic/gin"
 	calibreAPI "github.com/jianyun8023/calibre-api/src"
 	"github.com/spf13/viper"
 	"log"
+	"net/http"
+	"net/url"
 	"strings"
 )
 
 var db = make(map[string]string)
 
-func setupRouter(client *calibreAPI.CalibreApi) *gin.Engine {
+func setupRouter(c *calibreAPI.CalibreApi) *gin.Engine {
 	// Disable Console Color
 	// gin.DisableConsoleColor()
 	r := gin.Default()
 
-	r.GET("/get/cover/:id", client.GetCover)
-	r.GET("/get/book/:id", client.GetBookFile)
-	r.GET("/read/:id/toc", client.GetBookToc)
-	r.GET("/read/:id/file/*path", client.GetBookContent)
+	r.GET("/get/cover/:id", c.GetCover)
+	r.GET("/get/book/:id", c.GetBookFile)
+	r.GET("/read/:id/toc", c.GetBookToc)
+	r.GET("/read/:id/file/*path", c.GetBookContent)
 	//r.GET("/read/:id/file", client.GetBookFile)
-	r.GET("/book/:id", client.GetBook)
-	r.GET("/search", client.Search)
-	r.POST("/search", client.Search)
+	r.GET("/book/:id", c.GetBook)
+	r.GET("/search", c.Search)
+	r.POST("/search", c.Search)
+
+	lanzouAPI, _ := lanzou.New(&client.Config{})
+	r.GET("/api/lanzou", func(c *gin.Context) {
+		lanzouUrl := c.Query("url")
+		pwd := c.Query("pwd")
+
+		_, err := url.Parse(lanzouUrl)
+		if err != nil {
+			c.JSON(http.StatusBadRequest,
+				&Response{
+					Msg:  "url参数错误",
+					Code: 401,
+				})
+			return
+		}
+
+		link, err := lanzouAPI.ResolveShareURL(lanzouUrl, pwd)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError,
+				&Response{
+					Msg:  err.Error(),
+					Code: 500,
+				})
+			return
+		}
+		c.JSON(http.StatusOK, &Response{
+			Msg:  err.Error(),
+			Code: 500,
+			Data: link,
+		})
+	})
 	//r.StaticFS("/css", http.Dir("./static/css"))
 	//r.StaticFS("/js", http.Dir("./static/js"))
 	//r.StaticFile("/favicon.ico", "./static/favicon.ico")
@@ -40,8 +75,8 @@ func main() {
 	} else {
 		gin.SetMode(gin.ReleaseMode)
 	}
-	var client = calibreAPI.NewClient(conf)
-	r := setupRouter(&client)
+	var c = calibreAPI.NewClient(conf)
+	r := setupRouter(&c)
 	r.Run(conf.Address)
 }
 
@@ -67,4 +102,10 @@ func initConfig() *calibreAPI.Config {
 	marshal, _ := json.Marshal(conf)
 	log.Printf("loaded config %s", marshal)
 	return &conf
+}
+
+type Response struct {
+	Code int64                 `json:"code"`
+	Data []lanzou.ResponseData `json:"data"`
+	Msg  string                `json:"msg"`
 }
