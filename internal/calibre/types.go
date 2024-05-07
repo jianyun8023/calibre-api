e68bad685cc4ed07a6f7e594229eeb03ba6c5a58
Jianyun <yihy8023@gmail.com>
2024-05-07 20:23:36 +0800

    支持读取calibre sqlite库更新索引

--- internal/calibre/types.go
+++ internal/calibre/types.go
@@ -1,18 +1,16 @@
 package calibre
 
 import (
+	"database/sql"
 	"time"
 )
 
 type Book struct {
-	AuthorSort  string   `json:"author_sort"`
-	Authors     string   `json:"authors"`
-	Cover       string   `json:"cover"`
-	Formats     []string `json:"formats"`
-	ID          int64    `json:"id"`
-	Identifiers struct {
-		MobiAsin string `json:"mobi-asin"`
-	} `json:"identifiers"`
+	AuthorSort   string    `json:"author_sort"`
+	Authors      string    `json:"authors"`
+	Cover        string    `json:"cover"`
+	FilePath     string    `json:"file_path"`
+	ID           int64     `json:"id"`
 	Isbn         string    `json:"isbn"`
 	Languages    []string  `json:"languages"`
 	LastModified time.Time `json:"last_modified"`
@@ -21,9 +19,26 @@ type Book struct {
 	SeriesIndex  float64   `json:"series_index"`
 	Size         int64     `json:"size"`
 	Tags         []string  `json:"tags"`
-	Timestamp    time.Time `json:"timestamp"`
 	Title        string    `json:"title"`
-	UUID         string    `json:"uuid"`
+}
+
+type BookRaw struct {
+	AuthorSort   string         `json:"author_sort"`
+	Authors      string         `json:"authors"`
+	Cover        string         `json:"cover"`
+	FilePath     string         `json:"file_path"`
+	ID           int64          `json:"id"`
+	Isbn         sql.NullString `json:"isbn"`
+	Languages    []string       `json:"languages"`
+	LastModified time.Time      `json:"last_modified"`
+	Pubdate      time.Time      `json:"pubdate"`
+	Publisher    sql.NullString `json:"publisher"`
+	SeriesIndex  float64        `json:"series_index"`
+	Size         int64          `json:"size"`
+	Tags         []string       `json:"tags"`
+	Timestamp    time.Time      `json:"timestamp"`
+	Title        string         `json:"title"`
+	UUID         string         `json:"uuid"`
 }
 
 type Config struct {
