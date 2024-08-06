c6c0bfdb3287627b50c33161f57fdbd9bdfa3885
Jianyun <yihy8023@gmail.com>
2024-08-06 17:44:10 +0800

    优化元数据搜索

--- app/calibre-pages/src/components/MetadataSearch.vue
+++ app/calibre-pages/src/components/MetadataSearch.vue
@@ -1,38 +1,42 @@
 <template>
 
-  <el-row>
-    <el-col :span="12">
-      <el-autocomplete
-          v-model="query"
-          :fetch-suggestions="querySearch"
-          clearable
-          class="w-50"
-          placeholder="Please Input"
-          @select="handleSelect"
-      >
-        <template #append>
-          <el-button @click="searchMetadata">搜索</el-button>
-        </template>
-      </el-autocomplete>
+  <div v-loading="querySearchLoading">
+    <el-row >
+      <el-col :span="12">
+        <el-autocomplete
+            v-model="query"
+            :fetch-suggestions="querySearch"
+            clearable
+            class="w-50"
+            placeholder="Please Input"
+            @select="handleSelect"
+        >
+          <template #append>
+            <el-icon @click="searchMetadata">
+              <Search/>
+            </el-icon>
+          </template>
+        </el-autocomplete>
 
-    </el-col>
-  </el-row>
-  <el-table :data="tableData" height="350" style="width: 100%" highlight-current-row
-            @current-change="handleCurrentChange">
-    <el-table-column label="封面" width="180">
-      <template #default="scope">
-        <el-image
-            style="width: 100px; height: 150px"
-            :src="'/api/proxy/cover/' + scope.row.image"
-            fit="cover"/>
-      </template>
-    </el-table-column>
-    <el-table-column prop="title" label="标题" width="180"/>
-    <el-table-column prop="author" label="作者" width="180"/>
-    <el-table-column prop="publisher" label="出版社"/>
-    <el-table-column prop="pubdate" label="发布日期"/>
-    <el-table-column prop="isbn13" label="ISBN"/>
-  </el-table>
+      </el-col>
+    </el-row>
+    <el-table :data="tableData" height="350" style="width: 100%" highlight-current-row
+              @current-change="handleCurrentChange">
+      <el-table-column label="封面" width="180">
+        <template #default="scope">
+          <el-image
+              style="width: 100px; height: 150px"
+              :src="'/api/proxy/cover/' + scope.row.image"
+              fit="cover"/>
+        </template>
+      </el-table-column>
+      <el-table-column prop="title" label="标题" width="180"/>
+      <el-table-column prop="author" label="作者" width="180"/>
+      <el-table-column prop="publisher" label="出版社"/>
+      <el-table-column prop="pubdate" label="发布日期"/>
+      <el-table-column prop="isbn13" label="ISBN"/>
+    </el-table>
+  </div>
 </template>
 <script>
 import {ElButton, ElInput, ElNotification} from 'element-plus'
@@ -48,6 +52,7 @@ export default {
   },
   data() {
     return {
+      querySearchLoading: false,
       selectRow: {},
       query: '',
       options: [],
@@ -87,25 +92,38 @@ export default {
   },
   methods: {
     async searchMetadata() {
+      this.querySearchLoading = true;
       if (this.query) {
-        const response = await fetch('/api/metadata/search?query=' + this.query, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-        })
-        const data = await response.json()
-
-        if (!data.success) {
-          ElNotification({
-            title: '未找到相关书籍',
-            message: this.query,
-            type: 'warning',
+        try {
+          const response = await fetch('/api/metadata/search?query=' + this.query, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json'
+            },
           })
+          const data = await response.json()
+          if (!data.success) {
+            ElNotification({
+              title: '未找到相关书籍',
+              message: this.query,
+              type: 'warning',
+            })
+          }
+          console.log(data)
+          this.tableData = data.books
+        } catch (e) {
+          ElNotification({
+            title: '搜索失败',
+            message: 'Error: ' + e,
+            type: 'error',
+          })
+          console.log(e)
+
         }
-        console.log(data)
-        this.tableData = data.books
       }
+      this.querySearchLoading = false;
+
+
     },
     handleCurrentChange(val) {
       this.$emit('current-metadata', val)
