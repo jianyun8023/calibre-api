3257ece2e0ed8aa51c36613d37794846e57a3387
Jianyun <yihy8023@gmail.com>
2024-08-08 15:17:29 +0800

    修复作为为空时的问题

--- app/calibre-pages/src/components/MetadataEdit.vue
+++ app/calibre-pages/src/components/MetadataEdit.vue
@@ -352,6 +352,9 @@ export default {
       }
     },
     arraysEqual(arr1: string[], arr2: string[]) {
+      if (!arr1) return false
+      if (!arr2) return false
+      if (arr1 === arr2) return true
       if (arr1.length !== arr2.length) return false
       for (let i = 0; i < arr1.length; i++) {
         if (arr1[i] !== arr2[i]) return false
