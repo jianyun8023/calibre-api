b0b3adcb5b7d6292ca5ddaf27f092c053f65b59e
Jianyun <yihy8023@gmail.com>
2024-08-16 10:11:09 +0800

    在线阅读页面

--- app/calibre-pages/src/App.vue
+++ app/calibre-pages/src/App.vue
@@ -4,8 +4,8 @@
       <SiteHeader />
     </el-header>
     <el-container>
-      <el-row>
-        <el-col :span="4" :sm="24" :xs="24" :lg="2">
+      <el-row style="width: 100%">
+        <el-col class="sidebar-parent" :span="4" :sm="24" :xs="24" :lg="2">
           <el-aside class="sidebar">
             <Sidebar />
           </el-aside>
@@ -52,5 +52,8 @@ export default {
     height: 100px;
     --el-aside-width: 100%;
   }
+}
+.sidebar-parent{
+
 }
 </style>
