ab7e1423364245387b9bf11599a8e2ba07037f07
Jianyun <yihy8023@gmail.com>
2024-08-16 10:11:09 +0800

    在线阅读页面

--- app/calibre-pages/src/views/ReadBook.vue
+++ app/calibre-pages/src/views/ReadBook.vue
@@ -1,32 +1,93 @@
 <template>
-  <el-row :gutter="20" justify="space-between" style='height: 100vh'>
-    <vue-reader :url="bookUrl"
-                :epubOptions='{
-      allowPopups: true, // Adds `allow-popups` to sandbox-attribute
-      allowScriptedContent: true, // Adds `allow-scripts` to sandbox-attribute
-  }'
+  <!--  <el-container class="full-height">-->
+  <!--    <el-col :span='24' class="full-height">-->
+  <div class="full-height">
+    <vue-reader ref="reader" :url="bookUrl"
+                :location="location"
+                :getRendition="getRendition"
+                @update:location="locationChange"
     />
-  </el-row>
+  </div>
+  <el-button @click="jumpToPath(initPath)">跳转</el-button>
+  <!--    </el-col>-->
+
+  <!--  </el-container>-->
 </template>
 <script lang="ts">
 import {VueReader} from 'vue-reader'
-import {ElRow} from "element-plus";
+import {useStorage} from '@vueuse/core'
+
+import {ElContainer, ElRow} from "element-plus";
 
 export default {
   name: 'ReadBook',
-  components: {ElRow, VueReader},
+  components: {ElContainer, ElRow, VueReader},
   data() {
     return {
       bookId: '',
       bookUrl: '',
+      initPath: null as string,
+      location: useStorage('book-progress', 0, undefined, {
+        serializer: {
+          read: (v) => JSON.parse(v),
+          write: (v) => JSON.stringify(v),
+        },
+      }),
+      rendition: {}
+
     }
   },
   created() {
+    // this.$refs.reader.getRendition
     this.bookId = (this.$route as any).params.id
     this.bookUrl = `/api/get/book/${this.bookId}.epub`
+    if (this.$route.query.path){
+      this.initPath = this.$route.query.path as string
+    }
   },
+  methods: {
+    locationChange: (epubcifi) => {
+      console.log(epubcifi)
+      location.value = epubcifi
+    },
+    jumpToPath(path: any) {
+      console.log(this.rendition)
+      this.$refs.reader.setLocation(path);
+    },
+    getRendition(val) {
+      this.rendition = val
+      this.rendition.themes.default({
+        '::selection': {
+          background: 'orange',
+        },
+      })
+      // this.renditionrendition.on('selected', setRenderSelection)
+    }
+  }
 }
 </script>
-<style scoped>
+<style>
+.full-height {
+  height: 90vh;
+  display: flex;
+  flex-direction: row;
+
+  .container {
+    width: 100%;
+  }
+}
+
+.sidebar {
+  display: none !important;
+}
+
+.el-aside {
+  display: none !important;
+}
+
+.sidebar-parent {
+  display: none !important;
+}
+
 
 </style>
